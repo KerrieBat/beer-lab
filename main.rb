2897338272f2b0f379009e3c5acf3864ec9b11ca
Paul Williams <paul@pdwilliams.net>
2016-11-01 17:47:23 +1100

    layout view

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ require_relative 'models/master_recipe'
 require_relative 'models/hop'
 require_relative 'models/fermentable'
 require_relative 'models/yeast'
-require_relative 'style'
+require_relative 'models/style'
 
 enable :sessions
 
